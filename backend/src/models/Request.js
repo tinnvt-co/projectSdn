6e7af3c92bd963c317eb72fe76bea4d9d503e59b
miten202 <mtiendz202@gmail.com>
2026-03-17 22:15:04 +0700

    up request

--- backend/src/models/Request.js
+++ backend/src/models/Request.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+// Mô hình dữ liệu cho yêu cầu của sinh viên
+// Mỗi yêu cầu sẽ thuộc về một sinh viên và có các thông tin như loại yêu cầu, tiêu đề, mô tả, tệp đính kèm, phòng hiện tại, phòng mục tiêu (nếu có), trạng thái xử lý, đánh giá của quản lý và đánh giá của quản trị viên.
 
 const requestSchema = new mongoose.Schema(
     {
